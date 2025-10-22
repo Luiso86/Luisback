19abaab95a307fd1da016789cc61da044f6985dc
Luis <luisonovoa86@gmail.com>
2025-10-22 00:33:41 -0300

    GET products

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(Process.argv);
+//obtener productos 
+
+let [, , method, resource] = process.argv;
+
+method = method.toUpperCase();
+resource = resource.toLowerCase();
+
+if (method == 'GET' && resource == 'products') {
+    // el await solo puede usarse dentro de funciones asincronas 
+const response = await fetch('https://fakestoreapi.com/products')
+const data = await response.jason() 
+console.log(data);
+}
