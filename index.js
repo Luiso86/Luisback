7fce26d7e7c6dd04f542a066e58a4f37d2e65dd8
Luis <luisonovoa86@gmail.com>
2025-10-22 01:16:23 -0300

    GET products/{id}

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ let [, , method, resource] = process.argv;
 method = method.toUpperCase();
 resource = resource.toLowerCase();
 
-if (method == 'GET' && resource == 'products') {
+if (method == 'GET' && resource.startsWith('product')) { 
+    //const [, id] = resource.split('/')[1]; 
+    let id = resource.split('/')[1]; //obtener el id del recurso
+    id = parseInt(id);
+
+    if (isNaN(id) || id <= 0) {
+        console.log('El id debe ser un numero');
+    }
+
+
+//if (method == 'GET' && resource == 'products') {
     // el await solo puede usarse dentro de funciones asincronas 
-const response = await fetch('https://fakestoreapi.com/products')
-const data = await response.jason() 
-console.log(data);
+//const response = await fetch('https://fakestoreapi.com/products')
+//const data = await response.jason() 
+//console.log(data);
+//}
+
+fetch('https://fakestoreapi.com/products/' + id)
+  .then(response => response.json())
+  .then(data => console.log(data));
 }
